762c835db8862fdf8749ee252ac8a76e3d4cc605
ns408 <nsrea408@gmail.com>
2020-01-10 00:13:05 +1100

    playing with randint and choice from random module in the python standard library

---
+++ python crash course/example_python_standard_library.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# function from random module: randint()
+
+from random import randint
+print(f"Random number between 1 and 6 on every run: {randint(1, 6)}")
+
+# choice function
+from random import choice
+players = ["dumdum", "dumdum1", "dumdum2", "dumdum3"]
+first_up = choice(players)
+print(f"Random players from the list using choice: {first_up}")
