9508a4f8c9ec3f40c2acf614955a01e44d831f22
ns408 <nsrea408@gmail.com>
2020-01-25 15:03:42 +1100

    classes finished

--- python crash course/exercises.py
+++ python crash course/exercises.py
@@ -65,3 +65,88 @@ lottery.print_ticket()
 9-15. Lottery Analysis: You can use a loop to see how hard it might be to win the kind of lottery you just modeled. Make a list or tuple called my_ticket. Write a loop that keeps pulling numbers until your ticket wins. Print a message reporting how many times the loop had to run to give you a winning ticket.
 """
 
+my_ticket = ("b", 74, "m", 98)
+
+"""
+Run the loop to generate a random ticket.
+Check if elements of the ticket match my_ticket.
+Keep a count of the loop.
+"""
+
+count = 0
+while True:
+    count += 1
+    gen_ticket = Lottery(alist)
+    new_ticket = gen_ticket.select_ticket()
+    if my_ticket == new_ticket:
+        print("found the ticket")
+        break
+    elif count >= 1_000_000:
+        print("Count exceeded 1_000_000. Breaking out of the loop.")
+        break
+print()
+# So much better code from https://ehmatthes.github.io/pcc_2e/solutions/chapter_9/#9-15-lottery-analysis
+
+from random import choice
+
+def get_winning_ticket(possibilities):
+    """Return a winning ticket from a set of possibilities."""
+    winning_ticket = []
+
+    # Don't get the chars repeated
+    while len(winning_ticket) < 4:
+        pulled_item = choice(possibilities)
+
+        # add items that aren't already pulled
+        if pulled_item not in winning_ticket:
+            winning_ticket.append(pulled_item)
+    return winning_ticket
+
+def check_ticket(played_ticket, winning_ticket):
+    # check all elments in the played ticket. If not in winning ticket, then False
+    # This doesn't seem to check the sequence and focuses on just having the elements which could be in any order.
+    # Exercises content doesn't specify that.
+    for element in played_ticket:
+        if element not in winning_ticket:
+            return False
+
+    # We must have a winning ticket
+    return True
+
+def make_random_ticket(possibilities):
+    """Return a random ticket from a set of possibilities."""
+    ticket = []
+    # Don't get the chars repeated
+    while len(ticket) < 4:
+        pulled_item = choice(possibilities)
+
+        # add items that aren't already pulled
+        if pulled_item not in ticket:
+            ticket.append(pulled_item)
+    return ticket
+
+possibilities = list(range(1,11)) + [chr(x) for x in range(ord('a'), ord('e') + 1)]
+winning_ticket = get_winning_ticket(possibilities)
+
+plays = 0
+won = False
+
+# max tries
+max_tries = 1_000_000
+
+while not won:
+    new_ticket = make_random_ticket(possibilities)
+    won = check_ticket(new_ticket, winning_ticket)
+    plays += 1
+    if plays >= max_tries:
+        break
+
+if won:
+    print("We have a winning ticket!")
+    print(f"Your ticket: {new_ticket}")
+    print(f"Your ticket: {winning_ticket}")
+    print(f"It only took {plays} tries to win!")
+else:
+    print(f"Tried {plays} times, without pulling a winner. :(")
+    print(f"Your ticket: {new_ticket}")
+    print(f"Winning ticket: {winning_ticket}")
