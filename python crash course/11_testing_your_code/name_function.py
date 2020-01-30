299592e1e1c1493b76076d548ddbfaf3bd17200a
ns408 <nsrea408@gmail.com>
2020-01-31 00:20:47 +1100

    example: failed test
    ```
    $ /shared/repositories/Github_ns408/practice_python/.env/bin/python /Users/user/.vscode/extensions/ms-python.python-2020.1.58038/pythonFiles/lib/python/new_ptvsd/no_wheels/ptvsd/launcher "/shared/repositories/Github_ns408/practice_python/python crash course/11_testing_your_code/test_name_function.py"
    E
    ======================================================================
    ERROR: test_first_last_name (__main__.NamesTestCase)
    Test names like Jim Rohn.
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/shared/repositories/Github_ns408/practice_python/python crash course/11_testing_your_code/test_name_function.py", line 15, in test_first_last_name
        formatted_name = get_formatted_name('jim', 'rohn')
    TypeError: get_formatted_name() missing 1 required positional argument: 'last'
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.000s
    
    FAILED (errors=1)
    ```

--- python crash course/11_testing_your_code/name_function.py
+++ python crash course/11_testing_your_code/name_function.py
@@ -4,7 +4,7 @@
 Testing a function
 """
 
-def get_formatted_name(first, last):
+def get_formatted_name(first, middle, last):
     """Generate a neatly formatted full name."""
-    full_name = f"{first} {last}"
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
