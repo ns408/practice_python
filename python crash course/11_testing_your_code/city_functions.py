d5808caa65123d51eddc0bca9c35d23d3140406b
ns408 <nsrea408@gmail.com>
2020-01-31 02:07:52 +1100

    fix the function to pass the test

--- python crash course/11_testing_your_code/city_functions.py
+++ python crash course/11_testing_your_code/city_functions.py
@@ -5,9 +5,12 @@ Function that accepts two parameters: a city name and a country name.
 Returns a single string of the form 'City, Country'. Eg: 'Santiago, Chile'
 """
 
-def get_city_country(city, country, population):
+def get_city_country(city, country, population=""):
     """Return formatted string 'City, Country -  population xxx'."""
-    city_country = f"{city.title()}, {country.title()} - population {population}"
+    city_country = f"{city}, {country}"
+    city_country = city_country.title()
+    if population:
+        city_country = f"{city_country} - population {population}"
     return city_country
 
 """
