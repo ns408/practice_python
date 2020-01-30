61c282db98df888aaba36b8f41f09d50160e8dcb
ns408 <nsrea408@gmail.com>
2020-01-31 02:04:17 +1100

    make the test fail

--- python crash course/11_testing_your_code/city_functions.py
+++ python crash course/11_testing_your_code/city_functions.py
@@ -5,7 +5,13 @@ Function that accepts two parameters: a city name and a country name.
 Returns a single string of the form 'City, Country'. Eg: 'Santiago, Chile'
 """
 
-def get_city_country(city, country):
-    """Return formatted string 'City, Country'."""
-    city_country = f"{city}, {country}"
-    return city_country.title()
+def get_city_country(city, country, population):
+    """Return formatted string 'City, Country -  population xxx'."""
+    city_country = f"{city.title()}, {country.title()} - population {population}"
+    return city_country
+
+"""
+ 11-2. Population: Modify your function so it requires a third parameter, population. It should now return a single string of the form City, Country – population xxx, such as Santiago, Chile – population 5000000. Run test _cities.py again. Make sure test_city_country() fails this time.
+Modify the function so the population parameter is optional. Run test _cities.py again, and make sure test_city_country() passes again.
+Write a second test called test_city_country_population() that veri- fies you can call your function with the values 'santiago', 'chile', and 'population=5000000'. Run test_cities.py again, and make sure this new test passes.
+"""
