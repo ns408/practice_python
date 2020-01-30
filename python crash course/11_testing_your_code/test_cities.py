f04b8ca193ebc76a18d28fd5c31f67c19cfe5f42
ns408 <nsrea408@gmail.com>
2020-01-31 02:12:03 +1100

    added another test.
    Tested that it passes.

--- python crash course/11_testing_your_code/test_cities.py
+++ python crash course/11_testing_your_code/test_cities.py
@@ -16,5 +16,10 @@ class CitiesTestCase(unittest.TestCase):
         city_country = get_city_country('santiago', 'chile')
         self.assertEqual(city_country, 'Santiago, Chile')
 
+    def test_city_country_population(self):
+        """test the function"""
+        city_country = get_city_country('santiago', 'chile', 5000000)
+        self.assertEqual(city_country, 'Santiago, Chile - population=5000000')
+
 if __name__ == "__main__":
     unittest.main()
