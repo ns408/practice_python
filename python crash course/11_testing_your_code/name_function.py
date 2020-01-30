3eea9b3af8a0d55d107ebe996263f095ea6e4f3b
ns408 <nsrea408@gmail.com>
2020-01-31 00:35:27 +1100

    responding to the failed test. fixed the function.

--- python crash course/11_testing_your_code/name_function.py
+++ python crash course/11_testing_your_code/name_function.py
@@ -4,7 +4,10 @@
 Testing a function
 """
 
-def get_formatted_name(first, middle, last):
+def get_formatted_name(first, last, middle=''):
     """Generate a neatly formatted full name."""
-    full_name = f"{first} {middle} {last}"
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
     return full_name.title()
