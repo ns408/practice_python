42c80a416f40df9f94cbe8e3e3bc53bb3935dc94
ns408 <nsrea408@gmail.com>
2020-05-30 02:13:57 +1000

    Define Model

--- projects/project_learning_log/learning_logs/models.py
+++ projects/project_learning_log/learning_logs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# Structure: User will create number of topics.
+# Entries tied to a topic and displayed as text.
+
+class Topic(models.Model):
+    """A topic for user"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return string representation of the model."""
+        return self.text
