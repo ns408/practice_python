83d7027215589c67235ef7e1dc549939415e8539
ns408 <nsrea408@gmail.com>
2020-06-30 00:12:54 +1000

    adding basics to 01_basics.py

--- book_python crash course/01_basics.py
+++ book_python crash course/01_basics.py
@@ -5,3 +5,34 @@ print(message)
 
 message = "sup yo!"
 print(message)
+
+
+"""
+name.py
+Change each word to begin with capital letters.
+title() is a method.
+"""
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+"""
+Concatenate strings
+"""
+first_name = "batman"
+last_name = "nunya"
+full_name = first_name + " " + last_name
+print("\nSup " + full_name.title())
+
+"""
+tabs
+newlines
+"""
+print("This is a \ttabbed string with a \nnewline.")
+
+"""
+Strip whitespace
+"""
+print("Demonstrate use of rstrip, lstrip and strip")
+print("strip1 :" + "strip1 ".lstrip())
