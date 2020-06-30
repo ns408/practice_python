1504a198de01d212cf645631fa9737b7031dd586
ns408 <nsrea408@gmail.com>
2020-07-01 00:00:58 +1000

    fun with basics - again

--- book_python crash course/01_basics.py
+++ book_python crash course/01_basics.py
@@ -34,5 +34,25 @@ print("This is a \ttabbed string with a \nnewline.")
 """
 Strip whitespace
 """
-print("Demonstrate use of rstrip, lstrip and strip")
-print("strip1 :" + "strip1 ".lstrip())
+print("\nDemonstrate use of rstrip, lstrip and strip")
+print("lstrip: " + " string".lstrip())
+print("rstrip: " + "string ".rstrip())
+print("strip:  " + " string ".strip())
+
+# Numbers
+print(f"\nExponents:\n3 ** 3 = {3 ** 3}\n")
+
+print("Arbitrary number of decimal places due to how numbers are present internally.")
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+
+# str() function
+message = "Sup " + "k" + str(8)
+print( "\nUsing str(): " + message)
+
+value = 3 / 2
+print("\nDivision of integers: " + str(value))
+
+print("import this\nThe Zen of Python, by Tim Peters\n\n")
+import this
